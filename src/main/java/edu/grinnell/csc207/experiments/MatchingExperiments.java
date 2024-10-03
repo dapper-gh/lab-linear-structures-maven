237256f5433b23e8257b0528eaa96a8f1e18d75f
David Stroud <>
2024-10-03 08:49:08 -0500

    Fix bugs in ArrayBasedStack and MatchingExperiments

--- src/main/java/edu/grinnell/csc207/experiments/MatchingExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/MatchingExperiments.java
@@ -1,6 +1,7 @@
 package edu.grinnell.csc207.experiments;
 
 import java.io.PrintWriter;
+import edu.grinnell.csc207.util.StringUtils;
 
 /**
  * Experiments with matching parentheses and other such things.
@@ -20,7 +21,7 @@ public class MatchingExperiments {
     pen.print("checkMatching(\"" + str + "\") = ");
     pen.flush();
     try {
-      pen.println(checkMatching(str));
+      pen.println(StringUtils.checkMatching(str));
     } catch (Exception e) {
       pen.println("*** ERROR *** " + e.toString());
     } // try/catch
