ba04c3f9722598c36aba2902df091769caf69e9a
David Stroud <>
2024-10-08 08:59:15 -0500

    Fix .get() method in ArrayBasedQueue

--- src/main/java/edu/grinnell/csc207/linear/ArrayBasedQueue.java
+++ src/main/java/edu/grinnell/csc207/linear/ArrayBasedQueue.java
@@ -79,8 +79,6 @@ public class ArrayBasedQueue<T> implements Queue<T> {
     // Grab and clear the element at the front of the queue
     T result = this.values[this.front];
     this.values[this.front++] = null;
-    // We're removing an element, so decrement the size
-    --this.size;
     // And we're done
     return result;
   } // get(T)
